1a4715e7d38cbfc5dbbc98b0de45f82685a83822
Soumya Subhakant Jena <Subhasoumya20@gmail.com>
2022-06-05 14:11:52 +0530

    1

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose')
+// import mongoose from 'mongoose'
+const URI="mongodb://localhost:27017"
+
+const connectToMongo=()=>{
+    mongoose.connect(URI, ()=>{
+        console.log("connected to mongo successfully")
+    })
+}
+
+
+// module.exports=connectToMongo;
+
+// export {connectToMongo}
+
+module.exports=connectToMongo;
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-import express from "express";
+const connectToMongo=require('./db.js');
+const express = require('express')
+// import {connectToMongo} from './db.js'
+connectToMongo();
 
+
+
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
