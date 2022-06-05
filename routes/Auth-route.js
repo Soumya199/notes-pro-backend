dc75cecd4c8d1e45c97df50f5424ea2172933f8b
Soumya Subhakant Jena <Subhasoumya20@gmail.com>
2022-06-05 17:58:10 +0530

    3

--- routes/Auth-route.js
+++ routes/Auth-route.js
@@ -1,11 +1,13 @@
 const express=require("express");
+const { body } = require("express-validator");
 const User = require("../models/User");
 
 const router=express.Router();
 
 //Create a User using : POST "/api/auth/". Dosesn't require Auth
 
-router.post("/",(req,res)=>{
+router.post("/",
+(req,res)=>{
   console.log(req.body);
   const user=User(req.body)
   user.save()
