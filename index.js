2c8aac6b5efc78c1998e6962e55a36a097aca845
Soumya Subhakant Jena <Subhasoumya20@gmail.com>
2022-06-05 19:07:37 +0530

    5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const connectToMongo=require('./db.js');
-const express = require('express')
+const connectToMongo = require("./db.js");
+const express = require("express");
 // import {connectToMongo} from './db.js'
 connectToMongo();
 
@@ -9,10 +9,9 @@ const port = 4000;
 app.use(express.json());
 
 //Available Routes
-app.use("/api/auth",require("./routes/Auth-route"))
-app.use("/api/notes",require("./routes/Notes-route"))
+app.use("/api/auth", require("./routes/Auth-route"));
+app.use("/api/notes", require("./routes/Notes-route"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
--- routes/Auth-route.js
+++ routes/Auth-route.js
@@ -1,56 +1,52 @@
-const express=require("express");
-const { body,validationResult} = require("express-validator");
+const express = require("express");
+const { body, validationResult } = require("express-validator");
 const res = require("express/lib/response");
 const User = require("../models/User");
 
-
-const router=express.Router();
+const router = express.Router();
 
 //Create a User using : POST "/api/auth/createuser". No Login Require
 
-router.post("/createuser",
-body('name').isLength({ min: 3 }),
-body('email').isEmail(),
-body('password').isLength({ min: 5 }),
-async(req,res)=>{
-  try{
-  //If there are errors then send bad request with message
-  const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    
-    //Check wheather user exist with same mail id
+router.post(
+  "/createuser",
+  body("name").isLength({ min: 3 }),
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  async (req, res) => {
+    try {
+      //If there are errors then send bad request with message
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
-    console.log(req.body.email);
-    let userExist= await User.findOne({email:req.body.email});
-    if(userExist){
-      return res.status(400).json({ errors: `User with ${req.body.email} mailid already exist`});
+      //Check wheather user exist with same mail id
+
+      console.log(req.body.email);
+      let userExist = await User.findOne({ email: req.body.email });
+      if (userExist) {
+        return res
+          .status(400)
+          .json({ errors: `User with ${req.body.email} mailid already exist` });
+      } else {
+        //Creating user  in database
+        let user = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        });
+        res.json(user);
+      }
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("some error occured, please try after some time");
     }
-    else{
-      //Creating user  in database
-     let user=await User.create({
-        name: req.body.name,
-        email:req.body.email,
-        password: req.body.password,
-      })
-      res.json(user);
-    }
+
+    // console.log(req.body);
+    // const user=User(req.body)
+    // user.save()
+    // res.send("hello")
   }
-  catch(error){
-    console.log(error.message);
-    res.status(500).send("some error occured, please try after some time")
-  }
-  
-    
-    
-  // console.log(req.body);
-  // const user=User(req.body)
-  // user.save()
-  // res.send("hello")
-}
-)
+);
 
-
-
-module.exports=router;
+module.exports = router;
